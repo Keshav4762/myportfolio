acbdb1b7ecf3fe2248be4ebe70d208e7514c3320
Keshav Kartik Sharma <keshav4762@gmail.com>
2025-01-13 23:36:49 +0530

    Edited 1

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,6 +10,7 @@ const Projects = () => {
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
             <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:-100}} transition={{duration:1}} className="w-full lg:w-1/4 ">
+            <a href={project.link} target='_blank'>
               <img
                 src={project.image}
                 width={150}
@@ -17,9 +18,10 @@ const Projects = () => {
                 alt=""
                 className="mb-6 rounded"
               />
+              </a>
             </motion.div>
 
-            <motion.div whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1}} className="w-full max-w-xl lg:w-3/4">
+            <motion.div  whileInView={{opacity:1, x:0}} initial={{opacity:0, x:100}} transition={{duration:1}} className="w-full max-w-xl lg:w-3/4 content-center ">
               <h6 className="mb-2 font-semibold">{project.title}</h6>
               <p className="mb-4 text-neutral-400">{project.description}</p>
               {project.technologies.map((tech, index) => (
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a motivated and adaptable full-stack developer eager to build efficient and user-friendly web applications. As a fresher, I have gained hands-on experience working with technologies like React, Next.js, Node.js, MySQL, and MongoDB through personal projects and learning endeavors. My journey into web development stems from a strong curiosity about how things work, and I am excited to grow this into a dynamic career where I can continually learn and embrace new challenges. I thrive in collaborative settings, enjoy tackling complex problems, and am committed to delivering quality solutions. Beyond coding, I enjoy staying active, exploring emerging technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -40,25 +40,27 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "E-Commerce Website",
+    title: "STL Book (Chrome extension)",
     image: project1,
     description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+      "A chrome extension that helps competitive programmers while solving DSA questions.",
+    technologies: ["HTML", "CSS", "Javascript"],
+    link:'https://chromewebstore.google.com/detail/stl-book/adkojnocmaplcpiejookomnjcpclgcai?hl=en'
   },
   {
-    title: "Task Management App",
+    title: "kk-coins (Crypto Tracker App)",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "An application for tracking crypto investments with help of graphs.",
+    technologies: ["HTML", "CSS", "React", "Firebase"],
+    link: 'https://kk-coins.vercel.app/',
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "React", "Tailwind CSS"],
   },
   {
     title: "Blogging Platform",
